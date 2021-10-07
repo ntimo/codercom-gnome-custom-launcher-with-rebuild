2701830ba42acc0bb2912bc35734e90356ba5db3
ntimo <git@nowitzki.me>
2021-10-07 16:44:43 +0000

    Show rebuild process, and allow to start multiple coder apps

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 ## Synopsis
 This custom desktop shortcut was created since coder currently can't start a workspace that is turned off, and will then run into an error if you just try to start code server using the normal chrome pwa. This shortcut fixes this by using the coderc cli to start the workspace before launching chrome.
 
+This can be used with multiple applications that you have in your coder workspace for example VSCode or PyCharm
+
 This has been tested with Zorin OS 16 (Ubuntu 20.04).
 
 ## Requierments:
@@ -14,9 +16,9 @@ This has been tested with Zorin OS 16 (Ubuntu 20.04).
 - Login to coder using `coder login` in your terminal
 - Start google chrome and setup the normal PWA for code server using coder [docs link with how to](https://coder.com/docs/coder/v1.23/workspaces/pwa)
 - Grep the chrome app id from the .desktop file chrome created in `~/.local/share/applications/chrome*.desktop` the file will list `NAME=coder`
-- Find a nice looking VSCode icon on google and download it to `~/.local/share/icons/coder.png`
+- Find a nice looking VSCode icon on google and download it to `~/.local/share/icons/coder/<application name>.png`
 - Copy the desktop file from this repo to `~/.local/share/applications/VSCode.desktop`
-- - Update the file with the icon path and the path to the start_coder.sh
+  - Update the file with the icon path and the path to the start_coder.sh
+  - Update the args of the start_coder.sh with the `<application name>` followed by the `<workspace name>` and then the `<chrome app id>`
 - Copy the start_coder.sh to your home directory and make it executable `chmod +x start_coder.sh`
-- - Edit the start_coder.sh and fill in your workspace name and the chrome app id from earlier
 - Try it out and have fun.
--- start_coder.sh
+++ start_coder.sh
@@ -6,33 +6,67 @@
 #              It also prints nice help messages.
 
 
-WS_NAME=""
-CHROME_APP_ID=""
-CODER_ICON="$HOME/.local/share/icons/coder.png"
+CODER_APPLICATION=$1
+WS_NAME="$2"
+CHROME_APP_ID="$3"
+CODER_ICON="$HOME/.local/share/icons/coder/$CODER_APPLICATION.png"
 
-WS_EXISTS=0
-while read -r line ; do
-  # parse workspace name and status from coder cmd
-  ws=$(cut -d ";" -f1 <<< $line)
-  status=$(cut -d ";" -f2 <<< $line)
-  if [ "$ws" == "$WS_NAME" ]; then
-    # workspace exists
-    WS_EXISTS=$(($WS_EXISTS + 1))
-    if [ "$status" == "OFF" ];then
-      notify-send -i "$CODER_ICON" "Coder" "Workspace $WS_NAME is turned off"
-      coder workspaces rebuild --force "$WS_NAME"
-      notify-send -i "$CODER_ICON" "Coder" "Workspace $WS_NAME is beeing rebuild"
-      gnome-terminal -- bash -c "coder workspaces watch-build "$WS_NAME""
-      notify-send -i "$CODER_ICON" "Coder" "Worspace $WS_NAME rebuild. Starting Chrome.."
-      sleep 2
-      /opt/google/chrome/google-chrome --profile-directory=Default --app-id=$CHROME_APP_ID
-    else
-      notify-send -i "$CODER_ICON" "Coder" "Workspace $WS_NAME is turned on. Starting Chrome.."
-      /opt/google/chrome/google-chrome --profile-directory=Default --app-id=$CHROME_APP_ID
+if [ $CODER_APPLICATION == "" ]; then
+  notify-send "Coder" "CODER_APPLICATION can't be empty please add it to your .desktop file"
+  exit 1
+fi
+if [ $WS_NAME == "" ]; then
+  notify-send "Coder" "WS_NAME can't be empty please add it to your .desktop file"
+  exit 1
+fi
+if [ $CHROME_APP_ID == "" ]; then
+  notify-send "Coder" "CHROME_APP_ID can't be empty please  add it to your .desktop file"
+  exit 1
+fi
+
+function get_ws_status() {
+  while read -r line ; do
+    ws=$(cut -d ";" -f1 <<< $line)
+    status=$(cut -d ";" -f2 <<< $line)
+    if [ "$ws" == "$WS_NAME" ]; then
+      WS_STATUS=$status
     fi
-  fi
-done < <(coder workspaces ls | awk '{ print $1 ";" $6 }')
+  done < <(coder workspaces ls | awk '{ print $1 ";" $6 }')
+  return 0
+}
 
-if [ $WS_EXISTS == 0 ]; then
+function check_if_ws_exists() {
+  WS_EXISTS="1"
+  while read -r line ; do
+    ws=$(cut -d ";" -f1 <<< $line)
+    status=$(cut -d ";" -f2 <<< $line)
+    if [ "$ws" == "$WS_NAME" ]; then
+      WS_EXISTS="0"
+    fi
+  done < <(coder workspaces ls | awk '{ print $1 ";" $6 }')
+  return 0
+}
+
+check_if_ws_exists
+if [ "$WS_EXISTS" == "1" ]; then
   notify-send -i "$CODER_ICON" "Coder" "Workspace $WS_NAME was not found, please make sure the workspace exists"
+  exit 1
+fi
+
+get_ws_status
+if [ "$WS_STATUS" == "OFF" ];then
+    notify-send -i "$CODER_ICON" "Coder" "Workspace $WS_NAME is turned off"
+    coder workspaces rebuild --force "$WS_NAME"
+    notify-send -i "$CODER_ICON" "Coder" "Workspace $WS_NAME is beeing rebuild"
+    gnome-terminal --hide-menubar --geometry=170x40 --title="Workspace rebuild log: '$WS_NAME'" -- bash -c "coder workspaces watch-build "$WS_NAME" && exit"
+    while [ "$WS_STATUS" != "ON" ]; do
+      get_ws_status
+      sleep 1
+    done
+    notify-send -i "$CODER_ICON" "Coder" "Worspace $WS_NAME rebuild. Starting Chrome.."
+    sleep 2
+    /opt/google/chrome/google-chrome --profile-directory=Default --app-id=$CHROME_APP_ID
+  else
+    notify-send -i "$CODER_ICON" "Coder" "Workspace $WS_NAME is turned on. Starting Chrome.."
+    /opt/google/chrome/google-chrome --profile-directory=Default --app-id=$CHROME_APP_ID
 fi
